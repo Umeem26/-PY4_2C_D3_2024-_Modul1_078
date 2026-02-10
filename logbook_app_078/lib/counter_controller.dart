3dde97303575c97bc7c765926ec0e9a2ee4159ba
Umeem26 <hisyamk.u5062@gmail.com>
2026-02-10 09:46:56 +0700

    Fix : Task 2

--- logbook_app_078/lib/counter_controller.dart
+++ logbook_app_078/lib/counter_controller.dart
@@ -1,27 +1,38 @@
 class CounterController {
   int _counter = 0;
-  int _step = 1; // Variabel Step
+  int _step = 1;
+  final List<String> _history = [];
 
   int get value => _counter;
   int get step => _step;
+  List<String> get history => _history;
 
-  void setStep(int newStep) {
-    _step = newStep;
+  void _addHistory(String message) {
+    _history.insert(0, message);
+    if (_history.length > 5) {
+      _history.removeLast();
+    }
   }
 
+  void setStep(int newStep) => _step = newStep;
+
   void increment() {
-    _counter += _step; // Tambah sesuai step
+    _counter += _step;
+    _addHistory("Ditambah $_step (Total: $_counter)");
   }
 
   void decrement() {
     if (_counter >= _step) {
-      _counter -= _step; // Kurang sesuai step
+      _counter -= _step;
+      _addHistory("Dikurang $_step (Total: $_counter)");
     } else {
       _counter = 0;
+      _addHistory("Dikurang mentok ke 0");
     }
   }
 
   void reset() {
     _counter = 0;
+    _addHistory("Data di-reset ke 0");
   }
 }
